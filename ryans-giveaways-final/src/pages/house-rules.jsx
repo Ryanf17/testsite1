ca1238be0f4f154fa2c31a5463e8ad0dab24b889
Ryanf17 <ryan.fontenot24@gmail.com>
2025-07-03 19:41:58 -0500

    Add files via upload

---
+++ ryans-giveaways-final/src/pages/house-rules.jsx
@@ -0,0 +1,4 @@
+import React from 'react';
+export default function HouseRules() {
+  return <div><h1>House Rules</h1><ul><li>No outside alcohol</li><li>Quiet hours: 10PM – 7AM</li><li>Clean up after yourself</li></ul></div>;
+}
---
+++ ryans-giveaways-final/src/pages/index.jsx
@@ -0,0 +1,4 @@
+import React from 'react';
+export default function Home() {
+  return <div><h1>Welcome to Ryan's Giveaways!</h1><p>Join the latest giveaways, check the bar, and more.</p></div>;
+}
