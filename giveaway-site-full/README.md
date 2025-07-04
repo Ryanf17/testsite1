13799dedddbfb4a3d80a80975fcf800873fc7bb8
Ryanf17 <ryan.fontenot24@gmail.com>
2025-07-03 19:33:01 -0500

    Add files via upload

---
+++ giveaway-site-full/README.md
@@ -0,0 +1,2 @@
+# Ryan's Giveaway Website
+A React app with admin editing for bar menu and giveaway access codes.
---
+++ giveaway-site-full/src/pages/giveaway.jsx
@@ -0,0 +1,4 @@
+import React from 'react';
+export default function Giveaway() {
+  return <div><h1>Giveaway</h1><p>Access code required to enter.</p></div>;
+}
